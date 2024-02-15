f00b477b3512f70f65039140e44d0a7dc5238cff
timwlee <timothyl@adobe.com>
2024-02-15 14:49:48 -0800

    Switch american flag icon

--- blocks/header-topnav/header-topnav.js
+++ blocks/header-topnav/header-topnav.js
@@ -16,7 +16,7 @@ export default async function decorate(block) {
         alt: 'Account User Icon',
       },
       {
-        iconSrc: '/icons/american-flag-2.png',
+        iconSrc: '/icons/american-flag.png',
         text: 'usa - en',
         wrapperClass: 'language-wrapper',
         iconClass: 'flag-icon',
