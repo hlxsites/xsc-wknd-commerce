77e716962fcb2cd0063987c6d9b1345ee034bcb6
timwlee <timothyl@adobe.com>
2024-02-24 15:12:59 -0800

    Test

--- blocks/destinations/destinations.js
+++ blocks/destinations/destinations.js
@@ -1,57 +1,59 @@
 export default function decorate(block) {
-  const link = block.querySelector('a.button');
-  const divElements = block.querySelectorAll('div');
-  let country = '';
-  let data = [];
 
-  divElements.forEach((divElement, index) => {
-    if (divElement.textContent.trim().toLowerCase() === 'country') {
-      if (divElements[index + 1]) {
-        country = divElements[index + 1].textContent.trim();
-      } else {
-        console.log('No country value found.');
-      }
-    }
-  });
+  console.log)("block: ", block);
+  // const link = block.querySelector('a.button');
+  // const divElements = block.querySelectorAll('div');
+  // let country = '';
+  // let data = [];
 
-  function modifyHTML() {
-    block.innerHTML = '';
+  // divElements.forEach((divElement, index) => {
+  //   if (divElement.textContent.trim().toLowerCase() === 'country') {
+  //     if (divElements[index + 1]) {
+  //       country = divElements[index + 1].textContent.trim();
+  //     } else {
+  //       console.log('No country value found.');
+  //     }
+  //   }
+  // });
 
-    data.forEach((item) => {
-      const createdCard = document.createElement('div');
-      createdCard.classList.add('wide-card');
-      createdCard.innerHTML = `
-        <div class="card-image">
-          <img loading="lazy" alt="" src="${item.image}" width="100" height="100">
-        </div>
-        <div class="card-info">
-          <h2>${item.name}</h2>
-          <p>${item.description}</p>
-          <p class="button-container"><a href="${item.cta}" title="View trips" class="custom-link">View trips</a></p>
-        </div>
-      `;
-      block.append(createdCard);
-    });
-  }
+  // function modifyHTML() {
+  //   block.innerHTML = '';
 
-  async function initialize() {
-    const response = await fetch(link?.href);
+  //   data.forEach((item) => {
+  //     const createdCard = document.createElement('div');
+  //     createdCard.classList.add('wide-card');
+  //     createdCard.innerHTML = `
+  //       <div class="card-image">
+  //         <img loading="lazy" alt="" src="${item.image}" width="100" height="100">
+  //       </div>
+  //       <div class="card-info">
+  //         <h2>${item.name}</h2>
+  //         <p>${item.description}</p>
+  //         <p class="button-container"><a href="${item.cta}" title="View trips" class="custom-link">View trips</a></p>
+  //       </div>
+  //     `;
+  //     block.append(createdCard);
+  //   });
+  // }
 
-    if (response.ok) {
-      const jsonData = await response.json();
-      data = jsonData?.data;
+  // async function initialize() {
+  //   const response = await fetch(link?.href);
 
-      if (country) {
-        const countries = jsonData?.raw.data;
-        const foundCountry = countries.find((obj) => obj.name === country);
-        data = [foundCountry];
-        modifyHTML();
-      } else {
-        data = jsonData?.data;
-        modifyHTML();
-      }
-    }
-  }
+  //   if (response.ok) {
+  //     const jsonData = await response.json();
+  //     data = jsonData?.data;
 
-  initialize();
+  //     if (country) {
+  //       const countries = jsonData?.raw.data;
+  //       const foundCountry = countries.find((obj) => obj.name === country);
+  //       data = [foundCountry];
+  //       modifyHTML();
+  //     } else {
+  //       data = jsonData?.data;
+  //       modifyHTML();
+  //     }
+  //   }
+  // }
+
+  // initialize();
 }
