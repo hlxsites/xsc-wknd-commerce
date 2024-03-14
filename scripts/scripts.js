f784664b5072f17d94cdc9cc5c08f3778bebf6ec
Dirk Rudolph <drudolph@adobe.com>
2024-03-14 21:39:31 +0100

    fix: await template to complete loading
    Otherwise loadLazy stats already and the template loads in parallel causing a flicker and layout shift

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -241,7 +241,7 @@ async function loadEager(doc) {
 
   const main = doc.querySelector('main');
   if (main) {
-    decorateTemplates(main);
+    await decorateTemplates(main);
     decorateMain(main);
     aggregateTabSectionsIntoComponents(main);
     document.body.classList.add('appear');
