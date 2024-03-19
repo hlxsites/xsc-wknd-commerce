b064b1a6f8bddbc4acb24ea876ab63e01e0ecedd
Stephen Rugh <rugh@adobe.com>
2024-03-19 11:17:32 -0500

    adds adyen ui integration and render code
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -7,12 +7,15 @@ import { events } from '@dropins/tools/event-bus.js';
 
 // Drop-in APIs
 import * as checkout from '@dropins/storefront-checkout/api.js';
+import * as adyen from '@dropins/adyen-checkout-extension/api.js';
 
 // Drop-in Providers
 import { render as provider } from '@dropins/storefront-checkout/render.js';
+import { render as extensionRenderer } from '@dropins/adyen-checkout-extension/render.js';
 
 // Drop-in Containers
 import Checkout from '@dropins/storefront-checkout/containers/Checkout.js';
+import AdyenPaymentMethod from '@dropins/adyen-checkout-extension/containers/AdyenPaymentMethod.js';
 
 export default async function decorate(block) {
   // If cartId is cached in session storage, use
@@ -21,10 +24,13 @@ export default async function decorate(block) {
 
   // Initialize Drop-ins
   initializers.register(checkout.initialize, {});
+  initializers.register(adyen.initialize, { environment: 'test' });
 
   // Listen for order confirmation and redirect to order confirmation page
   events.on('checkout/order', (data) => {
-    window.location.replace(`/order-confirmation?orderRef=${data.masked_order_id}`);
+    window.location.replace(
+      `/order-confirmation?orderRef=${data.masked_order_id}`,
+    );
   });
 
   return provider.render(Checkout, {
@@ -33,17 +39,13 @@ export default async function decorate(block) {
     routeCart: () => '/cart',
     slots: {
       PaymentMethods: async (context) => {
-        context.addPaymentMethodHandler('checkmo', {
+        context.addPaymentMethodHandler('adyen_cc', {
           render: (ctx, element) => {
             if (element) {
               // clear the element first
               element.innerHTML = '';
+              extensionRenderer.render(AdyenPaymentMethod, ctx)(element);
             }
-
-            // Optionally, create and render some custom content here.
-            // const $content = document.createElement('div');
-            // $content.innerText = 'Custom Check / Money order handler';
-            // ctx.appendHTMLElement($content);
           },
         });
       },
