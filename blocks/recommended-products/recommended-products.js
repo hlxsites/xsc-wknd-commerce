a0aa932708c00908add9d3c4949fdc1d28a4735f
AdobeAEMXSC <104858147+AdobeAEMXSC@users.noreply.github.com>
2024-03-19 10:49:38 -0400

    Update recommended-products.js
    Anchor around image

--- blocks/recommended-products/recommended-products.js
+++ blocks/recommended-products/recommended-products.js
@@ -20,7 +20,7 @@ export default async function decorate(block) {
     cards.push(jsx`
       <div class="slider-item ${index === activeIndex ? 'active' : ''}">
         <div class="slider-image">
-          <div class="image-wrapper">${pic.outerHTML}</div>
+          <a href="${item.path}"><div class="image-wrapper">${pic.outerHTML}</div></a>
         </div>
         <div class="slider-text">
           <a href="${item.path}">${item.name}</a>
