5ab38d3aa53719ffb28ef3925058b86ba352ea87
timwlee <timothyl@adobe.com>
2024-03-19 21:41:06 -0700

    Update adventure-details.js

--- blocks/adventure-details/adventure-details.js
+++ blocks/adventure-details/adventure-details.js
@@ -20,7 +20,7 @@ import { createAccordion, generateListHTML, getBlockPlaceholderInfo, buildAdvent
 
 export default async function decorate(block) {
   const placeholderObject = getBlockPlaceholderInfo(block);
-  const carouselControl = placeholderObject['Carousel-Controls'];
+  const carouselControl = placeholderObject['Carousel-Controls'].layout;
   block.innerHTML = '';
 
   buildAdventureBreadcrumbs();
@@ -49,13 +49,13 @@ export default async function decorate(block) {
       Title: (ctx) => {
         const headerTitle = document.createElement('div');
         headerTitle.className = 'custom-title';
-        headerTitle.textContent = placeholderObject['Header-Title'];
+        headerTitle.textContent = placeholderObject['Header-Title'].text;
         ctx.prependSibling(headerTitle);
       },
       Quantity: (ctx) => {
         const label = document.createElement('div');
         label.className = 'quantity-label';
-        label.textContent = placeholderObject['Quantity-Label'];
+        label.textContent = placeholderObject['Quantity-Label'].text;
         ctx.prependChild(label);
       },
       Actions: (ctx) => {
