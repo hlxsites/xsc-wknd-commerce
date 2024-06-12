b872265f55e88110c0e40131547af98e62c8740f
timwlee <timothyl@adobe.com>
2024-06-12 12:49:36 -0700

    Remove unneeded blocks

--- blocks/beat-the-buzzer-banner/beat-the-buzzer-banner.js
+++
@@ -1,6 +0,0 @@
-export default async function decorate(block) {
-  const firstChildDiv = block.querySelector('div:first-child');
-  const innerChildDiv = firstChildDiv.querySelector('div');
-  firstChildDiv.classList.add('banner-container');
-  innerChildDiv.classList.add('content');
-}
